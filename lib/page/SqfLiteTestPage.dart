2bc38e8fb80a72416a29bbde4f2c142ee8a7f625
bdmin <wenmq47@gmail.com>
2019-08-15 00:20:16 +0800

    Add sqflite plugin for sql database support of next plan.

--- lib/page/SqfLiteTestPage.dart
+++ lib/page/SqfLiteTestPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:wenmq_first_flickr_flutter_app/base/sql_support.dart';
 
 class SqfLiteTestPage extends StatefulWidget {
