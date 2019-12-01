2ea3d7bb1e0e3e7a03b85281e0a4df3c58891596
hellowmq <wenmq47@gmail.com>
2019-12-01 22:35:28 +0800

    [ADD] 2019-12-01 22:35:17 user SliverAppbar

--- lib/page/common_test/TestMyListViewPage.dart
+++ lib/page/common_test/TestMyListViewPage.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:wenmq_first_flickr_flutter_app/base/base_tool.dart';
 
-import 'package:wenmq_first_flickr_flutter_app/base/view/MyListView.dart';
+import 'package:wenmq_first_flickr_flutter_app/base/view/MyListView.dart'
+    as myList;
 import 'package:wenmq_first_flickr_flutter_app/base/view/MyScrollPhysics.dart';
 
 class TestMyListViewPage extends StatefulWidget {
@@ -21,6 +22,7 @@ class _TestMyListViewPageState extends State<TestMyListViewPage> {
 
   @override
   Widget build(BuildContext context) {
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.build),
@@ -33,12 +35,30 @@ class _TestMyListViewPageState extends State<TestMyListViewPage> {
           });
         },
       ),
-      body: buildMyBoxScrollView(),
+      body: new CustomScrollView(
+        slivers: List<Widget>.generate(
+          100,
+          (index) =>
+              _buildAppBar(context, statusBarHeight, title: index.toString()),
+        ),
+
+//        <Widget>[
+//          _buildAppBar(context, statusBarHeight),
+//          _buildAppBar(context, statusBarHeight),
+//          _buildAppBar(context, statusBarHeight),
+//          _buildAppBar(context, statusBarHeight),
+//          _buildAppBar(context, statusBarHeight),
+//          _buildAppBar(context, statusBarHeight),
+//          _buildAppBar(context, statusBarHeight),
+//          _buildAppBar(context, statusBarHeight),
+//          _buildAppBar(context, statusBarHeight),
+//        ],
+      ),
     );
   }
 
-  MyGridView buildMyGridView() {
-    return MyGridView.builder(
+  myList.MyGridView buildMyGridView() {
+    return myList.MyGridView.builder(
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         mainAxisSpacing: 20.0,
@@ -62,8 +82,8 @@ class _TestMyListViewPageState extends State<TestMyListViewPage> {
     );
   }
 
-  MyBoxScrollView buildMyBoxScrollView() {
-    return MyBoxScrollView(
+  myList.MyBoxScrollView buildMyBoxScrollView() {
+    return myList.MyBoxScrollView(
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         mainAxisSpacing: 20.0,
@@ -88,5 +108,49 @@ class _TestMyListViewPageState extends State<TestMyListViewPage> {
         ),
       );
     });
+  }
+
+  Widget _buildAppBar(BuildContext context, double statusBarHeight,
+      {String title = "title"}) {
+    final _kAppbarHeight = 128.0;
+    return SliverAppBar(
+      pinned: false,
+      expandedHeight: _kAppbarHeight,
+      actions: <Widget>[
+        Builder(
+          builder: (BuildContext context) => IconButton(
+            icon: const Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: () {
+              ShowMessage.showSnackBarWithContext(context, "no supported");
+            },
+          ),
+        )
+      ],
+      flexibleSpace: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          final Size size = constraints.biggest;
+          final double appBarHeight = size.height - statusBarHeight;
+          final double t = (appBarHeight - kToolbarHeight) /
+              (_kAppbarHeight - kToolbarHeight);
+          final double extraPadding =
+              Tween<double>(begin: 10.0, end: 24.0).transform(t);
+          final double logoHeight = appBarHeight - 1.5 * extraPadding;
+          return Padding(
+            padding: EdgeInsets.only(
+              top: statusBarHeight + 0.5 * extraPadding,
+              bottom: extraPadding,
+            ),
+            child: Center(
+              child: Container(
+                padding: EdgeInsets.all(8.0),
+                color: CommonBuilder.getRandomColor(),
+                child: Text(title),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
