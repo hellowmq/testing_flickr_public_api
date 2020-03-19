883a499727772e110c1a6f72057ba1223484579d
hellowmq <wenmq47@gmail.com>
2020-03-19 23:27:21 +0800

    [ADD] 2020-03-19 23:27:07 add sign method

--- lib/base/sign_tools.dart
+++ lib/base/sign_tools.dart
@@ -3,16 +3,26 @@ import 'dart:convert';
 import 'package:crypto/crypto.dart';
 
 class SignUtils {
-  static String hmacSha1Sign(String key, String baseString) {
+  static String hmacShaSign(Hash hash, String key, String baseString) {
     try {
-      var hmacSha1 = new Hmac(sha1, utf8.encode(key));
+      var hmacHash = new Hmac(hash, utf8.encode(key));
       print(baseString);
       String signature = Uri.encodeComponent(
-          base64.encode(hmacSha1.convert(utf8.encode(baseString)).bytes));
+          base64.encode(hmacHash
+              .convert(utf8.encode(baseString))
+              .bytes));
       return signature;
     } catch (e) {
       print(e);
       throw FormatException('Generate Signature Error');
     }
   }
+
+  /// Sha1 is probably unsafe. You should use Sha256 unless the protocol assign
+  /// Sha1 as the only sign method.
+  static String hmacSha1Sign(String key, String baseString) =>
+      hmacShaSign(sha1, key, baseString);
+
+  String hmacSha256Sign(String key, String baseString) =>
+      hmacShaSign(sha256, key, baseString);
 }
