28a5fdee81e53e396f276cf185adf70726e9a4e4
hellowmq <wenmq47@gmail.com>
2020-03-30 19:38:40 +0800

    [ADD] 2020-03-30 19:37:07 add use Vector represent 2d point.

--- lib/base/bean/point.dart
+++ lib/base/bean/point.dart
@@ -1,10 +1,58 @@
+import 'dart:math';
+
+import 'package:ml_linalg/linalg.dart';
+import 'package:ml_linalg/vector.dart';
 import 'package:wenmq_first_flickr_flutter_app/base/tools/2d_utils.dart';
 
 class Point {
-  double x;
-  double y;
+  Vector pxy; // position vector
+  Vector vxy; // velocity vector
+  double get px => pxy[0];
+  set px(double x) => (pxy as Iterable<double>)[0] = x;
 
-  double get rou => TwoDUtils.XY2polarRou(x, y);
+  double get py => pxy[1];
 
-  double get theta => TwoDUtils.XY2polarTheta(x, y);
+  double get vx => vxy[0];
+
+  double get vy => vxy[1];
+
+  double get pr => vxy.norm();
+
+  set pr(double radius) {
+    if (pr > 0) {
+      pxy = Vector.fromList([radius * cos(ptheta), radius * sin(ptheta)]);
+    } else if (pr == 0) {
+      pxy = Vector.fromList([0.0, 0.0]);
+    } else {
+      throw Exception("radius cannot be negative!");
+    }
+  }
+
+  double get ptheta => TwoDUtils.XY2polarTheta(px, py);
+
+  set ptheta(double theta) {
+    pxy = Vector.fromList([pr * cos(theta), pr * sin(theta)]);
+  }
+
+  double get vr => vxy.norm();
+
+  set vr(double radius) {
+    if (vr > 0) {
+      vxy = Vector.fromList([radius * cos(vtheta), radius * sin(vtheta)]);
+    } else if (vr == 0) {
+      vxy = Vector.fromList([0.0, 0.0]);
+    } else {
+      throw Exception("radius cannot be negative!");
+    }
+  }
+
+  double get vtheta => TwoDUtils.XY2polarTheta(vx, vy);
+
+  set vtheta(double theta) {
+    vxy = Vector.fromList([vr * cos(theta), vr * sin(theta)]);
+  }
+
+  void step(double time) {
+        pxy = pxy + vxy * time;
+  }
 }
